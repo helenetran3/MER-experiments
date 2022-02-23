61d52f19bb84ef14c51d1ac32b0aec4d7146478c
tr.helene@hotmail.com <hetran@frontalhpc2020.local.isima.fr>
2022-02-23 13:48:56 +0100

    Update run_cluster.sh

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,3 +1,14 @@
 #!/bin/bash 
 
-python3 ./dataset_load.py
+#===============================================================================
+# exemples d'options
+ 
+#SBATCH --partition=court     # choix de la partition où soumettre le job
+#SBATCH --time=48:0:0         # temps max alloué au job (format = m:s ou h:m:s ou j-h:m:s)
+#SBATCH --ntasks=1            # nb de tasks total pour le job
+#SBATCH --cpus-per-task=1     # 1 seul CPU pour une task
+#SBATCH --mem=32000           # mémoire nécessaire (par noeud) en Mo
+ 
+#===============================================================================
+
+python3 ./test.py
