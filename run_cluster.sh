efeb3badd937c8a434407278e822097886240919
tr.helene@hotmail.com <tr.helene@hotmail.com>
2022-03-22 14:02:48 +0100

    Change parameters sbatch

--- run_cluster.sh
+++ run_cluster.sh
@@ -6,11 +6,11 @@
 #SBATCH --partition=court     # choix de la partition où soumettre le job
 #SBATCH --time=48:0:0         # temps max alloué au job (format = m:s ou h:m:s ou j-h:m:s)
 #SBATCH --ntasks=1            # nb de tasks total pour le job
-#SBATCH --cpus-per-task=1     # 1 seul CPU pour une task
-#SBATCH --mem=32000           # mémoire nécessaire (par noeud) en Mo
+#SBATCH --cpus-per-task=24    # nb CPU pour une task
+#SBATCH --mem=1000            # mémoire nécessaire (par noeud) en Mo
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=helene.tran@doctorant.uca.fr
  
 #===============================================================================
 
-python3 ./main.py
+python3 ./main.py -pickle_name cmu_mosei_aligned_text_no_averaged 
